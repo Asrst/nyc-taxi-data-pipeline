b5d057e52c31e1ae7cee445454b97edaf9bf0765
satya.adda <addasaiteja@gmail.com>
2023-02-26 18:50:23 +0530

    update queries used for assingment 4

--- big_query/queries.sql
+++ big_query/queries.sql
@@ -4,17 +4,25 @@ FROM `nyc-taxi-data-pipeline-250123.dbt_sadda.fact_trips`
 where extract(year from pickup_datetime) in (2019, 2020);
 
 
--- count fhv trips from 2019
+-- count total fhv trips in the staging (includes trips with unknown zones)
+SELECT count(*)
+FROM `nyc-taxi-data-pipeline-250123.dbt_sadda.stg_fhv_tripdata`
+where extract(year from pickup_datetime) = 2019;
+
+
+-- count fhv trips from 2019 inner joined with zones
 SELECT count(*)
 FROM `nyc-taxi-data-pipeline-250123.dbt_sadda.fact_fhv_trips`
 where extract(year from pickup_datetime) = 2019;
 
--- count fhv trips from 2019, month wise
+
+-- count fact fhv trips joined with zones in 2019, month wise
 SELECT extract(month from pickup_datetime), count(*)
 FROM `nyc-taxi-data-pipeline-250123.dbt_sadda.fact_fhv_trips`
 where extract(year from pickup_datetime) = 2019
 group by 1
 order by 2;
+
 
 -- count green & yellow taxi (service type) trips from 2019, 2020
 SELECT service_type, count(*)
